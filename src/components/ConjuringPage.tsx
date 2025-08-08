7541b9fd4b228a14cdcf7be97dbb115c3e0160f0
Sinclaire sebastian <80564295+SVEC-TECH@users.noreply.github.com>
2025-08-07 19:51:06 -0700

    Update ConjuringPage.tsx

--- src/components/ConjuringPage.tsx
+++ src/components/ConjuringPage.tsx
@@ -18,9 +18,7 @@ const ConjuringPage: React.FC<ConjuringPageProps> = ({ onContinue }) => {
         <div className="absolute inset-0 bg-black/70"></div>
       </div>
 
-      {/* Grain Effect */}
-      <div className="absolute inset-0 opacity-20 bg-[url('data:image/svg+xml,%3Csvg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg"%3E%3Cfilter id="noiseFilter"%3E%3CfeTurbulence type="fractalNoise" baseFrequency="0.9" numOctaves="4" stitchTiles="stitch"/%3E%3C/filter%3E%3Crect width="100%25" height="100%25" filter="url(%23noiseFilter)"/%3E%3C/svg%3E')]"></div>
-
+    
       {/* Content */}
       <div className="relative z-10 flex flex-col items-center justify-center min-h-screen px-8 text-center">
         {/* Main Title */}
